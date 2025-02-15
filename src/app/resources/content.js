0ba3c220095440f245a82dc798657b395cfcca00
Halim <halimjonraimjanov@gmail.com>
2025-02-15 17:10:16 +0600

    creating and updating website portfolio

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,15 +1,15 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Khalim",
+  lastName: "Raimzhanov",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "Full Stack Developer",
   avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "Asia/Bishkek", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Kyrgyz", "Russian", "Uzbek", "Tajik", "Turkish"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
@@ -17,8 +17,8 @@ const newsletter = {
   title: <>Subscribe to {person.firstName}'s Newsletter</>,
   description: (
     <>
-      I occasionally write about design, technology, and share thoughts on the intersection of
-      creativity and engineering.
+      I occasionally write about programming, technology, and share thoughts on
+      the intersection of code, architecture, and problem-solving.
     </>
   ),
 };
@@ -29,22 +29,22 @@ const social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system/nextjs-starter",
+    link: "https://github.com/HalimRaimjanov",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/halimraimjanov/",
   },
   {
-    name: "X",
-    icon: "x",
-    link: "",
+    name: "Instagram",
+    icon: "instagram",
+    link: "https://www.instagram.com/halim_raimjanov?igsh=MTVoOWZydmxpazN5cA%3D%3D&utm_source=qr",
   },
   {
     name: "Email",
     icon: "email",
-    link: "mailto:example@gmail.com",
+    link: "mailto:halimjonraimjanov@gmail.com",
   },
 ];
 
@@ -52,11 +52,17 @@ const home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Design engineer and builder</>,
+  headline: <>Full stack Developer</>,
   subline: (
     <>
-      I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive
-      <br /> user experiences. After hours, I build my own projects.
+      I'm Khalim, a Team Lead at <InlineCode>Mbulak</InlineCode>, where I
+      oversee code reviews,
+      <br />
+      develop projects related to banking services, and conduct technical
+      assessments.
+      <br />I ensure high-quality software development, optimize processes,
+      <br />
+      and guide my team in delivering efficient and scalable solutions.
     </>
   ),
 };
@@ -74,16 +80,22 @@ const about = {
   },
   calendar: {
     display: true,
-    link: "https://cal.com",
+    link: "mailto:halimjonraimjanov@gmail.com",
   },
   intro: {
     display: true,
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        Khalim is a results-driven Team Lead at Mbulak with over seven years of
+        experience in fintech and backend development. He specializes in
+        building scalable financial solutions, optimizing development processes,
+        and leading high-performing teams. His expertise spans project strategy,
+        technical evaluation, and fostering effective communication to drive
+        success. Passionate about guiding projects from concept to completion,
+        Khalim thrives on collaboration and problem-solving. With a strong
+        foundation in software product management, he is dedicated to creating
+        impactful and efficient technology solutions in the financial sector.
       </>
     ),
   },
@@ -92,17 +104,33 @@ const about = {
     title: "Work Experience",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "MBulak",
+        timeframe: "Sep 2024 - Present",
+        role: "Team Lead",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Lead the development of fintech solutions, optimizing banking
+            services and improving transaction processing speeds.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Conduct detailed code reviews, enhancing code quality and reducing
+            critical bugs.
+          </>,
+          <>
+            Implement optimized development workflows, increasing team
+            efficiency and reducing project delivery time.
+          </>,
+          <>
+            Conduct technical assessments for new projects, ensuring scalability
+            and performance optimization.
+          </>,
+          <>
+            Foster a collaborative engineering culture, improving cross-team
+            communication and productivity.
+          </>,
+          <>
+            Work with Jira for project tracking, sprint planning, and issue
+            management.
           </>,
         ],
         images: [
@@ -116,17 +144,170 @@ const about = {
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: "Mbulak",
+        timeframe: "Sep 2023 - Sep 2024",
+        role: "Senior Software Engineer",
         achievements: [
           <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
+            Developed and implemented key backend solutions for the company’s
+            financial products.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Optimized database and server processes, improving request
+            processing speeds.
+          </>,
+          <>
+            Worked on integrating Tunduk services to streamline and accelerate
+            the financing process for clients.
+          </>,
+          <>
+            Led the development of high-performance backend solutions for
+            financial services.
+          </>,
+          <>
+            Created and maintained advanced SQL procedures for critical
+            financial operations.
+          </>,
+          <>
+            Integrated external services via Python, enhancing automation and
+            data flow.
+          </>,
+          <>Improved system scalability and optimized backend architecture.</>,
+        ],
+        images: [],
+      },
+      {
+        company: "Mbulak",
+        timeframe: "Sep 2022 - Sep 2023",
+        role: "Middle Software Engineer",
+        achievements: [
+          <>
+            Designed and implemented complex SQL procedures for financial
+            calculations and analytics.
+          </>,
+          <>
+            Developed Python-based integrations with external financial and
+            government services.
+          </>,
+          <>
+            Optimized database queries to improve system performance and reduce
+            processing time.
+          </>,
+          <>
+            Worked closely with cross-functional teams to develop scalable
+            fintech solutions.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Mbulak",
+        timeframe: "July 2021 - Sep 2022",
+        role: "Junior Software Engineer",
+        achievements: [
+          <>
+            Developed and optimized SQL procedures for financial transactions
+            and reporting.
+          </>,
+          <>
+            Assisted in integrating third-party services using Python for
+            automation and data exchange.
+          </>,
+          <>
+            Collaborated with senior developers on backend architecture
+            improvements.
+          </>,
+          <>Gained experience in API development and system debugging.</>,
+        ],
+        images: [],
+      },
+      {
+        company: "Mbulak",
+        timeframe: "Mar 2021 - July 2021",
+        role: "Intern",
+        achievements: [
+          <>
+            Gained foundational experience in software development, focusing on
+            backend technologies.
+          </>,
+          <>
+            Assisted in writing SQL queries and stored procedures for financial
+            data processing.
+          </>,
+          <>
+            Learned and applied best practices in coding, debugging, and
+            software optimization.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Mbulak",
+        timeframe: "Apr 2020 - Mar 2021",
+        role: "Senior Call Center Operator",
+        achievements: [
+          <>
+            Supervised a team of operators, ensuring high-quality customer
+            service and adherence to company policies.
+          </>,
+          <>
+            Trained new employees, enhancing team performance and knowledge
+            retention.
+          </>,
+          <>
+            Handled escalated customer issues, improving resolution efficiency.
+          </>,
+          <>
+            Assisted in optimizing call scripts and workflow processes to
+            increase call efficiency.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Mbulak",
+        timeframe: "Sep 2018 - Apr 2020",
+        role: "Call Center Operator",
+        achievements: [
+          <>
+            Provided customer support, assisting clients with financial services
+            and loan applications.
+          </>,
+          <>
+            Processed customer requests efficiently, improving service response
+            times.
+          </>,
+          <>
+            Developed strong communication and problem-solving skills, handling
+            complex client inquiries.
+          </>,
+          <>
+            Maintained accurate customer records and assisted in resolving
+            disputes.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "S Parfum",
+        timeframe: "Aug 2017 - Sep 2018",
+        role: "Sales Associate",
+        achievements: [
+          <>
+            Advised customers on fragrances, providing personalized
+            recommendations based on their preferences.
+          </>,
+          <>
+            Met and exceeded monthly sales targets, contributing to overall
+            store revenue.
+          </>,
+          <>
+            Maintained product displays and ensured a high standard of store
+            presentation.
+          </>,
+          <>
+            Built strong customer relationships, enhancing brand loyalty and
+            repeat purchases.
           </>,
         ],
         images: [],
@@ -138,12 +319,28 @@ const about = {
     title: "Studies",
     institutions: [
       {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
+        name: "Kyrgyz-Russian Slavic University",
+        description: (
+          <>
+            <b>Master</b> of Science in Software Project Management
+            <br />
+            <b>GPA:</b> 4.7 / 5,
+            <br />
+            <b>Study Period:</b> 2020 – 2022.
+          </>
+        ),
       },
       {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
+        name: "Kyrgyz-Russian Slavic University",
+        description: (
+          <>
+            <b>Bachelor</b> of Science in Electronics and Nanoelectronics
+            <br />
+            <b>GPA:</b> 4.95 /5
+            <br />
+            <b>Study Period:</b> 2016 – 2020.
+          </>
+        ),
       },
     ],
   },
@@ -153,7 +350,9 @@ const about = {
     skills: [
       {
         title: "Figma",
-        description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+        description: (
+          <>Able to prototype in Figma with Once UI with unnatural speed.</>
+        ),
         // optional: leave the array empty if you don't want to display images
         images: [
           {
@@ -172,7 +371,9 @@ const about = {
       },
       {
         title: "Next.js",
-        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+        description: (
+          <>Building next gen apps with Next.js + Once UI + Supabase.</>
+        ),
         // optional: leave the array empty if you don't want to display images
         images: [
           {
